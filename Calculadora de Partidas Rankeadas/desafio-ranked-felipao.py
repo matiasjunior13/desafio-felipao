7f6a3b99b2561543a8956e0cfec51e5fca8f485f
Matias Junior <139413061+matiasjunior13@users.noreply.github.com>
2023-11-06 19:59:36 -0300

    desafio-ranked-felipao.py

--- Calculadora de Partidas Rankeadas/desafio-ranked-felipao.py
+++ Calculadora de Partidas Rankeadas/desafio-ranked-felipao.py
@@ -1,1 +1,27 @@
+vitorias = 53
+derrotas = 5
+
+
+def calcular_nivel(vitorias, derrotas):
+    saldo = vitorias - derrotas
+    
+    if vitorias < 10:
+        nivel = "Ferro"
+    elif 10 <= vitorias <= 20:
+        nivel = "Bronze"
+    elif 21 <= vitorias <= 50:
+        nivel = "Prata"
+    elif 51 <= vitorias <= 80:
+        nivel = "Ouro"
+    elif 81 <= vitorias <= 90:
+        nivel = "Diamante"
+    elif 91 <= vitorias <= 100:
+        nivel = "Lendário"
+    else:
+        nivel = "Imortal"
+    
+    return f"O Herói tem saldo de {saldo} está no nível de {nivel}"
+
+resultado = calcular_nivel(vitorias, derrotas)
+print(resultado)
 
